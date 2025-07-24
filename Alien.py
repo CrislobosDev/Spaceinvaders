c42fb23bff3e1400297cf1d1393d986fbe9523bb
Cristian Villalobos <cristianvillalobosvv@gmail.com>
2025-07-24 16:56:28 -0400

    first commit

---
+++ Alien.py
@@ -0,0 +1,18 @@
+from MiniGameEngine import GameObject
+
+
+class Alien(GameObject):
+    # inicializamos el Alien
+    def __init__(self, x, y):
+        super().__init__(x, y, "Recursos/Alien.png", "Alien")
+
+    #movimiento del Alien
+    
+        
+        
+
+    # manejamos las colisiones
+    def onCollision(self, dt, gobj):
+        if gobj.getTipo() == "Bullet":
+            self.destroy()
+            print("Alien:me dieron")
