fbbd9af0354ce999e90a97429b61173797a67a0e
Cristian Villalobos <cristianvillalobosvv@gmail.com>
2025-07-26 11:47:23 -0400

    Naves se mueven dentro de un rango horizontalmente

--- Alien.py
+++ Alien.py
@@ -1,35 +1,34 @@
-from MiniGameEngine import GameObject # Importa la clase GameObject del módulo MiniGameEngine
+from MiniGameEngine import GameObject
 
+class Alien(GameObject):
+    def __init__(self, x, y, movimiento_ancho=100):  # Puedes pasar opcionalmente el ancho de movimiento
+        super().__init__(x, y, "Recursos/Alien.png", "Alien")
+        self.speed = 1
+        self.direction = 1
 
-class Alien(GameObject): # Define la clase Alien que hereda de GameObject
-    # inicializamos el Alien
-    def __init__(self, x, y): # Constructor de la clase Alien, toma coordenadas x e y
-        super().__init__(x, y, "Recursos/Alien.png", "Alien") # Llama al constructor de la clase padre (GameObject) para inicializar el alien con su posición, imagen y tipo "Alien"
-        self.speed = 2 # Atributo para la velocidad horizontal del alien (2 unidades por frame)
-        self.direction = 1 # Atributo para la dirección de movimiento (1: derecha, -1: izquierda)
+        # Definir el rango de movimiento horizontal limitado
+        self.initial_x = x
+        self.min_x = x - movimiento_ancho / 2
+        self.max_x = x + movimiento_ancho / 2
 
-    # movimiento del Alien
-    def onUpdate(self, dt): # Método llamado en cada frame para actualizar el estado del alien, recibe el tiempo delta (dt)
-        x = self.getX() # Obtiene la coordenada x actual del alien
-        y = self.getY() # Obtiene la coordenada y actual del alien
-        world_width = self.getWorldWidth() # Obtiene el ancho del mundo del juego
-        alien_width = self.getWidth() # Obtiene el ancho del alien
+    def onUpdate(self, dt):
+        x = self.getX()
+        y = self.getY()
 
-        # Calcula la nueva posición x
-        x += self.speed * self.direction # Mueve el alien horizontalmente sumando (o restando) la velocidad según la dirección
+        # Mover al alien
+        x += self.speed * self.direction
 
-        # Cambia de dirección si llega a los bordes
-        if x + alien_width / 2 > world_width: # Comprueba si el alien ha alcanzado o superado el borde derecho de la pantalla
-            x = world_width - alien_width / 2 # Ajusta la posición del alien para que no se salga del borde derecho
-            self.direction = -1 # Cambia la dirección a izquierda (-1)
-        elif x - alien_width / 2 < 0: # Comprueba si el alien ha alcanzado o superado el borde izquierdo de la pantalla
-            x = alien_width / 2 # Ajusta la posición del alien para que no se salga del borde izquierdo
-            self.direction = 1 # Cambia la dirección a derecha (1)
+        # Cambiar de dirección si llega a los límites definidos
+        if x > self.max_x:
+            x = self.max_x
+            self.direction = -1
+        elif x < self.min_x:
+            x = self.min_x
+            self.direction = 1
 
-        self.setPosition(x, y) # Actualiza la posición del alien en el juego
+        self.setPosition(x, y)
 
-    # manejamos las colisiones
-    def onCollision(self, dt, gobj): # Método llamado cuando el alien colisiona con otro objeto (gobj), recibe el tiempo delta (dt) y el objeto con el que colisiona
-        if gobj.getTipo() == "Bullet": # Comprueba si el objeto con el que colisionó es de tipo "Bullet"
-            self.destroy() # Si colisiona con una bala, destruye el alien
-            print("Alien:me dieron") # Imprime un mensaje en la consola
+    def onCollision(self, dt, gobj):
+        if gobj.getTipo() == "Bullet":
+            self.destroy()
+            print("Alien: me dieron")
